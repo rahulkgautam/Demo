901efe715cd2fa0580568d266f5efb3e01ab7112
rahulkgautam <rahulkgautam125@gmail.com>
2023-07-07 08:45:46 +0530

    Test Project

---
+++ Demo/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+    class Calculator
+    {
+        public static void Calculate()
+        {
+            Console.WriteLine("Enter first Number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second Number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter third Number");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int mul = a * b * c;
+            Console.WriteLine("Multipication of Three numbers " + mul);
+        }
+    }
+}
